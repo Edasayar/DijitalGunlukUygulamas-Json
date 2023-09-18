bc9caed0cd7bff660c36bb812ebd1e5ef9497471
Eda Sayar <esedasayar@gmail.com>
2023-09-18 13:20:07 +0300

    DijitalGunluk
    Json kaydetme yöntemiyle yapılan proje tamamlandı

---
+++ DijitalGunlukUygulaması/RoniHocaProje/frmLogin.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace RoniHocaProje
+{
+	partial class frmLogin
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogin));
+			lblKullaniciAdi = new Label();
+			lblSifre = new Label();
+			txtKullaniciAdi = new TextBox();
+			btnGirisYap = new Button();
+			panel1 = new Panel();
+			btnYeniKayitOlustur = new Button();
+			lblDurum = new Label();
+			btnGizle = new Button();
+			txtSifre = new TextBox();
+			dateTimePicker1 = new DateTimePicker();
+			pictureBox1 = new PictureBox();
+			panel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+			SuspendLayout();
+			// 
+			// lblKullaniciAdi
+			// 
+			lblKullaniciAdi.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+			lblKullaniciAdi.ForeColor = Color.Black;
+			lblKullaniciAdi.Location = new Point(16, 92);
+			lblKullaniciAdi.Name = "lblKullaniciAdi";
+			lblKullaniciAdi.Size = new Size(179, 45);
+			lblKullaniciAdi.TabIndex = 0;
+			lblKullaniciAdi.Text = "Kullanıcı Adı : ";
+			// 
+			// lblSifre
+			// 
+			lblSifre.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+			lblSifre.Location = new Point(103, 156);
+			lblSifre.Name = "lblSifre";
+			lblSifre.Size = new Size(92, 43);
+			lblSifre.TabIndex = 1;
+			lblSifre.Text = "Şifre : ";
+			// 
+			// txtKullaniciAdi
+			// 
+			txtKullaniciAdi.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+			txtKullaniciAdi.Location = new Point(201, 96);
+			txtKullaniciAdi.Name = "txtKullaniciAdi";
+			txtKullaniciAdi.Size = new Size(200, 34);
+			txtKullaniciAdi.TabIndex = 2;
+			// 
+			// btnGirisYap
+			// 
+			btnGirisYap.BackgroundImage = (Image)resources.GetObject("btnGirisYap.BackgroundImage");
+			btnGirisYap.BackgroundImageLayout = ImageLayout.Stretch;
+			btnGirisYap.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+			btnGirisYap.Location = new Point(201, 221);
+			btnGirisYap.Name = "btnGirisYap";
+			btnGirisYap.Size = new Size(107, 46);
+			btnGirisYap.TabIndex = 4;
+			btnGirisYap.Text = "Giriş Yap";
+			btnGirisYap.UseVisualStyleBackColor = true;
+			btnGirisYap.Click += btnGirisYap_Click;
+			// 
+			// panel1
+			// 
+			panel1.BackgroundImage = (Image)resources.GetObject("panel1.BackgroundImage");
+			panel1.BackgroundImageLayout = ImageLayout.Stretch;
+			panel1.Controls.Add(btnYeniKayitOlustur);
+			panel1.Controls.Add(lblDurum);
+			panel1.Controls.Add(btnGizle);
+			panel1.Controls.Add(txtSifre);
+			panel1.Controls.Add(btnGirisYap);
+			panel1.Controls.Add(lblKullaniciAdi);
+			panel1.Controls.Add(txtKullaniciAdi);
+			panel1.Controls.Add(lblSifre);
+			panel1.Location = new Point(60, 35);
+			panel1.Name = "panel1";
+			panel1.Size = new Size(526, 370);
+			panel1.TabIndex = 5;
+			// 
+			// btnYeniKayitOlustur
+			// 
+			btnYeniKayitOlustur.BackgroundImage = (Image)resources.GetObject("btnYeniKayitOlustur.BackgroundImage");
+			btnYeniKayitOlustur.BackgroundImageLayout = ImageLayout.Stretch;
+			btnYeniKayitOlustur.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+			btnYeniKayitOlustur.Location = new Point(314, 221);
+			btnYeniKayitOlustur.Name = "btnYeniKayitOlustur";
+			btnYeniKayitOlustur.Size = new Size(124, 46);
+			btnYeniKayitOlustur.TabIndex = 9;
+			btnYeniKayitOlustur.Text = "Yeni Kayıt Oluştur";
+			btnYeniKayitOlustur.UseVisualStyleBackColor = true;
+			btnYeniKayitOlustur.Click += btnYeniKayitOlustur_Click;
+			// 
+			// lblDurum
+			// 
+			lblDurum.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+			lblDurum.Location = new Point(202, 270);
+			lblDurum.Name = "lblDurum";
+			lblDurum.Size = new Size(236, 37);
+			lblDurum.TabIndex = 6;
+			// 
+			// btnGizle
+			// 
+			btnGizle.BackgroundImage = (Image)resources.GetObject("btnGizle.BackgroundImage");
+			btnGizle.BackgroundImageLayout = ImageLayout.Stretch;
+			btnGizle.Location = new Point(407, 160);
+			btnGizle.Name = "btnGizle";
+			btnGizle.Size = new Size(31, 29);
+			btnGizle.TabIndex = 5;
+			btnGizle.UseVisualStyleBackColor = true;
+			btnGizle.MouseDown += btnGizle_MouseDown;
+			btnGizle.MouseUp += btnGizle_MouseUp;
+			// 
+			// txtSifre
+			// 
+			txtSifre.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+			txtSifre.Location = new Point(202, 160);
+			txtSifre.Name = "txtSifre";
+			txtSifre.Size = new Size(200, 34);
+			txtSifre.TabIndex = 3;
+			txtSifre.TextChanged += txtSifre_TextChanged;
+			// 
+			// dateTimePicker1
+			// 
+			dateTimePicker1.Location = new Point(64, 411);
+			dateTimePicker1.Name = "dateTimePicker1";
+			dateTimePicker1.Size = new Size(191, 27);
+			dateTimePicker1.TabIndex = 6;
+			// 
+			// pictureBox1
+			// 
+			pictureBox1.BackgroundImage = (Image)resources.GetObject("pictureBox1.BackgroundImage");
+			pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
+			pictureBox1.Location = new Point(39, 552);
+			pictureBox1.Name = "pictureBox1";
+			pictureBox1.Size = new Size(154, 136);
+			pictureBox1.TabIndex = 7;
+			pictureBox1.TabStop = false;
+			// 
+			// frmLogin
+			// 
+			AutoScaleDimensions = new SizeF(8F, 20F);
+			AutoScaleMode = AutoScaleMode.Font;
+			BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+			BackgroundImageLayout = ImageLayout.Stretch;
+			ClientSize = new Size(654, 746);
+			Controls.Add(pictureBox1);
+			Controls.Add(dateTimePicker1);
+			Controls.Add(panel1);
+			Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+			Name = "frmLogin";
+			Text = "Giriş Yap";
+			panel1.ResumeLayout(false);
+			panel1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+			ResumeLayout(false);
+		}
+
+		#endregion
+
+		private Label lblKullaniciAdi;
+		private Label lblSifre;
+		private TextBox txtKullaniciAdi;
+		private Button btnGirisYap;
+		private Panel panel1;
+		private TextBox txtSifre;
+		private Button btnGizle;
+		private DateTimePicker dateTimePicker1;
+		private PictureBox pictureBox1;
+		private Label lblDurum;
+		private Button btnYeniKayitOlustur;
+	}
+}
---
+++ DijitalGunlukUygulaması/RoniHocaProje/frmYeniKayitOlustur.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace RoniHocaProje
+{
+	partial class frmYeniKayitOlustur
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmYeniKayitOlustur));
+			txtYeniSifre = new TextBox();
+			lblYeniKullanici = new Label();
+			txtKYeniKullaniciAdi = new TextBox();
+			lblSifre = new Label();
+			b = new Button();
+			button1 = new Button();
+			SuspendLayout();
+			// 
+			// txtYeniSifre
+			// 
+			txtYeniSifre.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+			txtYeniSifre.Location = new Point(326, 220);
+			txtYeniSifre.Margin = new Padding(3, 4, 3, 4);
+			txtYeniSifre.Name = "txtYeniSifre";
+			txtYeniSifre.Size = new Size(270, 34);
+			txtYeniSifre.TabIndex = 12;
+			// 
+			// lblYeniKullanici
+			// 
+			lblYeniKullanici.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+			lblYeniKullanici.ForeColor = Color.Black;
+			lblYeniKullanici.Location = new Point(85, 157);
+			lblYeniKullanici.Name = "lblYeniKullanici";
+			lblYeniKullanici.Size = new Size(235, 38);
+			lblYeniKullanici.TabIndex = 9;
+			lblYeniKullanici.Text = "Yeni Kullanıcı Adı :";
+			// 
+			// txtKYeniKullaniciAdi
+			// 
+			txtKYeniKullaniciAdi.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+			txtKYeniKullaniciAdi.Location = new Point(326, 161);
+			txtKYeniKullaniciAdi.Margin = new Padding(3, 4, 3, 4);
+			txtKYeniKullaniciAdi.Name = "txtKYeniKullaniciAdi";
+			txtKYeniKullaniciAdi.Size = new Size(270, 34);
+			txtKYeniKullaniciAdi.TabIndex = 11;
+			// 
+			// lblSifre
+			// 
+			lblSifre.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+			lblSifre.Location = new Point(174, 220);
+			lblSifre.Name = "lblSifre";
+			lblSifre.Size = new Size(146, 39);
+			lblSifre.TabIndex = 10;
+			lblSifre.Text = "Yeni Şifre : ";
+			// 
+			// b
+			// 
+			b.BackgroundImage = (Image)resources.GetObject("b.BackgroundImage");
+			b.BackgroundImageLayout = ImageLayout.Stretch;
+			b.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+			b.Location = new Point(367, 262);
+			b.Margin = new Padding(3, 4, 3, 4);
+			b.Name = "b";
+			b.Size = new Size(229, 61);
+			b.TabIndex = 16;
+			b.Text = "Yeni Kayıt Oluştur";
+			b.UseVisualStyleBackColor = true;
+			b.Click += button1_Click;
+			// 
+			// button1
+			// 
+			button1.BackgroundImage = (Image)resources.GetObject("button1.BackgroundImage");
+			button1.BackgroundImageLayout = ImageLayout.Stretch;
+			button1.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+			button1.Location = new Point(446, 435);
+			button1.Margin = new Padding(3, 4, 3, 4);
+			button1.Name = "button1";
+			button1.Size = new Size(192, 49);
+			button1.TabIndex = 17;
+			button1.Text = "Giriş Sayfasına Dön";
+			button1.UseVisualStyleBackColor = true;
+			button1.Click += button1_Click_1;
+			// 
+			// frmYeniKayitOlustur
+			// 
+			AutoScaleDimensions = new SizeF(8F, 20F);
+			AutoScaleMode = AutoScaleMode.Font;
+			BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+			BackgroundImageLayout = ImageLayout.Stretch;
+			ClientSize = new Size(726, 572);
+			Controls.Add(button1);
+			Controls.Add(b);
+			Controls.Add(txtYeniSifre);
+			Controls.Add(lblYeniKullanici);
+			Controls.Add(txtKYeniKullaniciAdi);
+			Controls.Add(lblSifre);
+			Margin = new Padding(3, 4, 3, 4);
+			Name = "frmYeniKayitOlustur";
+			Text = "Yeni Kayıt Oluştur";
+			ResumeLayout(false);
+			PerformLayout();
+		}
+
+		#endregion
+		private TextBox txtYeniSifre;
+		private Label lblYeniKullanici;
+		private TextBox txtKYeniKullaniciAdi;
+		private Label lblSifre;
+		private Button b;
+		private Button button1;
+	}
+}
